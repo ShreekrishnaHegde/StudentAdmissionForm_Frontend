2dc85e9fd4a577dd3ecae6ee6eae7e222f1655d5
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-09-29 13:53:26 +0530

    Added Footer and Header

--- saf-frontend/src/App.jsx
+++ saf-frontend/src/App.jsx
@@ -4,13 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ListStudentComponent from './components/ListStudentComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <HeaderComponent/>
       <ListStudentComponent/>
+      <FooterComponent/>
     </>
   )
 }
