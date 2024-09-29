ad861edc8c023b6ec3d39966f98e76225cc6e5a0
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-09-29 14:16:05 +0530

    Configred Routes

--- saf-frontend/src/App.jsx
+++ saf-frontend/src/App.jsx
@@ -6,15 +6,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import ListStudentComponent from './components/ListStudentComponent'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <HeaderComponent/>
-      <ListStudentComponent/>
-      <FooterComponent/>
+      <BrowserRouter>
+        <HeaderComponent/>
+          <Routes>
+           <Route path='/' element= { <ListStudentComponent/>}></Route>
+           <Route path='/students' element= {<ListStudentComponent/>}></Route>
+          </Routes>
+      < ListStudentComponent/>
+        <FooterComponent/>
+      </BrowserRouter>
     </>
   )
 }
